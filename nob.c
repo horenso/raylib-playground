5c98b139969663d8dc2acf095c8c9c21984c78b1
Jannis Adamek <jannis.adamek@gmail.com>
2026-08-04 17:22:54 +0200

    Add compile with debug and release to nob script

--- nob.c
+++ nob.c
@@ -22,21 +22,46 @@ static bool collect_c_and_h_files(Nob_Walk_Entry entry) {
     return true;
 }
 
-static bool build(const char *input_path, const char *output_path) {
+enum Optimize { Optimize_Debug, Optimize_Release };
+
+typedef struct {
+    const char *input_file;
+    const char *src_dir;
+    Nob_Cmd extra_flags;
+    enum Optimize optimize;
+} BuildConfig;
+
+static bool build(BuildConfig cfg, const char *name) {
+    const char *subdir = cfg.optimize == Optimize_Release ? OUT_DIR "release/" : OUT_DIR "debug/";
+    if (!nob_mkdir_if_not_exists(subdir)) {
+        return false;
+    }
+
     Nob_Cmd cmd = {0};
 
     nob_cc(&cmd);
     nob_cc_flags(&cmd);
 
-    nob_cc_inputs(&cmd, input_path);
-    nob_cc_output(&cmd, output_path);
+    if (cfg.src_dir) {
+        nob_walk_dir(cfg.src_dir, collect_c_files, .data = &cmd);
+    } else if (cfg.input_file) {
+        nob_cc_inputs(&cmd, cfg.input_file);
+    }
 
+    if (cfg.optimize == Optimize_Release) {
+        nob_cmd_append(&cmd, "-O3");
+    } else {
+        nob_cmd_append(&cmd, "-g");
+    }
+
+    nob_cc_output(&cmd, nob_temp_sprintf("%s%s", subdir, name));
     nob_cmd_append(&cmd, "-I./raylib/include", "raylib/lib/libraylib.a", "-L../raylib", "-lm", "-lX11");
+    nob_da_append_many(&cmd, cfg.extra_flags.items, cfg.extra_flags.count);
 
     return nob_cmd_run(&cmd);
 }
 
-static bool format() {
+static void format() {
     Nob_Cmd cmd = {0};
     nob_cmd_append(&cmd, "clang-format", "-i");
     nob_walk_dir("src", collect_c_and_h_files, .data = &cmd);
@@ -48,29 +73,44 @@ static bool format() {
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
-    if (argc >= 2 && strcmp(argv[1], "format") == 0) {
+    const char *cmd = argc >= 2 ? argv[1] : "debug";
+
+    enum Optimize variants[2];
+    size_t variant_count = 0;
+
+    if (strcmp(cmd, "debug") == 0) {
+        variants[variant_count++] = Optimize_Debug;
+    } else if (strcmp(cmd, "release") == 0) {
+        variants[variant_count++] = Optimize_Release;
+    } else if (strcmp(cmd, "all") == 0) {
+        variants[variant_count++] = Optimize_Debug;
+        variants[variant_count++] = Optimize_Release;
+    } else if (strcmp(cmd, "format") == 0) {
         format();
         return 0;
+    } else {
+        nob_log(NOB_INFO, "Usage: %s [debug|release|all|format]", argv[0]);
+        return 1;
     }
 
     if (!nob_mkdir_if_not_exists(OUT_DIR)) {
         return 1;
     }
 
-    if (!build("src/pong/main.c", OUT_DIR "pong")) {
-        nob_log(NOB_ERROR, "Could not build pong");
-    }
+    for (size_t i = 0; i < variant_count; i++) {
+        enum Optimize opt = variants[i];
 
-    {
-        Nob_Cmd cmd = {0};
-        nob_cc(&cmd);
-        nob_cc_flags(&cmd);
-        nob_walk_dir("src/commander", collect_c_files, .data = &cmd);
-        nob_cc_output(&cmd, OUT_DIR "commander");
-        nob_cmd_append(&cmd, "-Isrc/commander", "-I./raylib/include", "-I/usr/include/freetype2", "raylib/lib/libraylib.a", "-L../raylib",
-                       "-lm", "-lX11", "-lcurl", "-lfreetype");
-        if (!nob_cmd_run(&cmd)) {
-            nob_log(NOB_ERROR, "Could not build commander");
+        if (!build((BuildConfig){.input_file = "src/pong/main.c", .optimize = opt}, "pong")) {
+            nob_log(NOB_ERROR, "Could not build pong");
+        }
+
+        {
+            Nob_Cmd commander_flags = {0};
+            nob_cmd_append(&commander_flags, "-Isrc/commander", "-I/usr/include/freetype2", "-lcurl", "-lfreetype");
+            if (!build((BuildConfig){.src_dir = "src/commander", .extra_flags = commander_flags, .optimize = opt},
+                       "commander")) {
+                nob_log(NOB_ERROR, "Could not build commander");
+            }
         }
     }
 
